6fdfe6df50539e83b46614b61672d5e294d6ade6
Darrell Rajendra Wibowo <11211028@student.itk.ac.id>
2022-06-16 10:34:21 +0800

    finishing database

--- src/Database.java
+++ src/Database.java
@@ -6,9 +6,12 @@ public class Database {
     private String nimUser;
     private char kelasUser;
     private int pointUser;
+    private String keteranganUser;
 
     private boolean dbConnected = false;
     private boolean dbDisconnect = false;
+    private boolean continueDB = false;
+    private boolean sudahAbsenDB = false;
 
     private int idNo;
     private String mataKuliah;
@@ -17,9 +20,8 @@ public class Database {
     private Timestamp waktuKelas;
     private ArrayList<Object[]> dataTabel = new ArrayList<Object[]>();
 
-    private String keteranganUser;
-
     private Timestamp waktuSekarang;
+    private Timestamp waktuSQL;
 
     public void DatabaseUserPass(String nim, String password) {
         try {
@@ -130,6 +132,122 @@ public class Database {
         }
     }
 
+    public void DatabaseWaktuSQL(String nim, Timestamp batasWaktu) {
+        long ti = batasWaktu.getTime();
+        long substracted = ti - 60 * 60 * 1000;
+
+        try {
+            String sqlTime = "SELECT * FROM absendata WHERE waktu BETWEEN '"+(new Timestamp(substracted))+"' AND '"+batasWaktu+"'";
+
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection condb = DriverManager.getConnection("jdbc:mysql://localhost:3306/tubesk10pboa", "root", "");
+            Statement stmdb = condb.createStatement();
+            ResultSet rsdb = stmdb.executeQuery(sqlTime);
+
+            while(rsdb.next()) {
+                this.waktuSQL = rsdb.getTimestamp("waktu");
+                this.mataKuliah= rsdb.getString("matakuliah");
+                continueDB = true;
+            }
+            condb.close();
+        }
+        catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void DatabaseLanjut(Timestamp waktuSpesifik) {
+        try {
+            String sqlPassword = "SELECT * FROM kelasdata WHERE waktu = '"+waktuSpesifik+"'";
+            String sqlKet = "SELECT * FROM absendata WHERE waktu = '"+waktuSpesifik+"'";
+
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection condb = DriverManager.getConnection("jdbc:mysql://localhost:3306/tubesk10pboa", "root", "");
+            Statement stamdb = condb.createStatement();
+            ResultSet rsdb = stamdb.executeQuery(sqlPassword);
+
+            Statement stmdb = condb.createStatement();
+            ResultSet rsadb = stmdb.executeQuery(sqlKet);
+
+            if(rsadb.next()) {
+                if(rsadb.getString("keterangan").equals("Hadir") || rsadb.getString("keterangan").equals("Izin") || rsadb.getString("keterangan").equals("Absen")) {
+                    sudahAbsenDB = true;
+                    System.out.println("True");
+                }
+                else {
+                    sudahAbsenDB = false;
+                    System.out.println("False");
+                }
+            }
+
+            while(rsdb.next()) {
+                this.passwordKelas = rsdb.getString("passwordkelas");
+            }
+
+            condb.close();
+        }
+        catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void DatabaseUserAbsen(String ket, Timestamp waktu, String userid) {
+        try {
+            String sqlAbsen = "UPDATE absendata SET keterangan = ? WHERE nim = '"+userid+"' AND waktu = '"+waktu+"'";
+
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection condb = DriverManager.getConnection("jdbc:mysql://localhost:3306/tubesk10pboa", "root", "");
+            PreparedStatement stmdb = condb.prepareStatement(sqlAbsen);
+
+            stmdb.setString(1, ket);
+            
+            stmdb.executeUpdate();
+
+            condb.close();
+        }
+        catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void PointSystem(String nim) {
+        try {  
+            String sqlInsert = "UPDATE logindata SET points = ? WHERE nim = '"+nim+"'";
+            String sqlLogin = "SELECT * FROM logindata WHERE nim='"+nim+"'";
+
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection condb = DriverManager.getConnection("jdbc:mysql://localhost:3306/tubesk10pboa", "root", "");
+
+            PreparedStatement stmdb = condb.prepareStatement(sqlInsert);
+
+            Statement stmadb = condb.createStatement();
+            ResultSet rsdb = stmadb.executeQuery(sqlLogin);
+
+            if(rsdb.next()) {
+                this.pointUser = rsdb.getInt("points");
+            }
+
+            stmdb.setInt(1, pointUser + 1);
+            
+            stmdb.executeUpdate();
+
+            condb.close();
+        }
+        catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public String passRandom() {
+        int len = 8;
+        String chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%&";
+        Random rnd = new Random();
+		StringBuilder sb = new StringBuilder(len);
+		for (int i = 0; i < len; i++)
+			sb.append(chars.charAt(rnd.nextInt(chars.length())));
+        System.out.println(sb.toString());
+        return sb.toString();
+    }
 
     public String getNamaUser() {
         return this.namaUser;
@@ -154,14 +272,22 @@ public class Database {
         return this.dataTabel;
     }
 
-    public String passRandom() {
-        int len = 8;
-        String chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%&";
-        Random rnd = new Random();
-		StringBuilder sb = new StringBuilder(len);
-		for (int i = 0; i < len; i++)
-			sb.append(chars.charAt(rnd.nextInt(chars.length())));
-        System.out.println(sb.toString());
-        return sb.toString();
+    public String getMataKuliahYangAda() {
+        return this.mataKuliah;
+    }
+
+    public Timestamp getWaktudariSQLAktif() {
+        return waktuSQL;
+    }
+
+    public boolean getBolehLanjut() {
+        return continueDB;
+    }
+
+    public String getPasswordKelas() {
+        return this.passwordKelas;
+    }
+    public boolean getSudahAbsenDB() {
+        return this.sudahAbsenDB;
     }
 }
